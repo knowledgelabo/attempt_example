d4f08739c5dd580060f3a072dd3d77aa0ca9889f
Mika <mika.mizuno@gmail.com>
2019-03-05 20:51:07 +0900

    Implement: set timezone, generators when rails g command

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,23 @@ module AttemptExample
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework :rspec,
+      fixture: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      controller_specs: false,
+      request_specs: true
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
