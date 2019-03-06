49074efea36077a6c7a6b33f62ae6cb5b3da9447
Mika <mika.mizuno@gmail.com>
2019-03-06 18:17:53 +0900

    attemp仕様

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -8,22 +8,24 @@ class Sale < ApplicationRecord
   def self.import(csv)
     csv_text = csv.read
     csv_utf = CSV.parse( Kconv.toutf8(csv_text) )
+
     header = csv_utf.first
-    fiscal_year_names = header[2..4]
+    fiscal_year_names = header[2..4]  #=> ['2017/03/31, '2018/03/31', '2019/03/31']
     fiscal_years = fiscal_year_names.map do | fy_name |
-                     end_at = fy_name.to_time.end_of_day
-                     start_at = (end_at - 1.year).tomorrow.beginning_of_day
+                     end_at = fy_name.to_time
+                     start_at = end_at - 12.months
                      FiscalYear.where(name: fy_name, start_at: start_at, end_at: end_at).first_or_create
                    end
-    csv_utf.slice!(0)
+
     csv_utf.each do | row |
       break if row[0] == '売上高合計'
       division = Division.where(name: row[0]).first_or_create
       member = Member.where(full_name: row[1], division: division).first_or_create
-      fiscal_years.each.with_index(1) do | fiscal_year, i |
+      i = 1
+      fiscal_years.each do | fiscal_year |
         sale = member.sales.where(fiscal_year: fiscal_year).first_or_create
-        amount = row[i+1].gsub(',', '').to_i
-        sale.update(amount: amount)
+        sale.update(amount: row[i+1])
+        i = i + 1
       end
     end
   end
